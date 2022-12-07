43dd2614bdc16d974a29e519dced0b0abb04cbb0
avmedvedskiy <a.v.medvedskiy@gmail.com>
2022-12-07 09:35:09 +0200

    add [MeansImplicitUse]

--- Runtime/InjectAttribute.cs
+++ Runtime/InjectAttribute.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using JetBrains.Annotations;
 
 namespace DI
 {
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Constructor | AttributeTargets.Method)]
+    [MeansImplicitUse]
     public class InjectAttribute : Attribute
     {
     }
